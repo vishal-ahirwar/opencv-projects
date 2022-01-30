254ecf2f2f481bc460644971dc49f5002b397199
IVishalAhirwar <vishalchoudhry0091@gmail.com>
2022-01-30 13:25:39 +0530

    main.cpp updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,16 @@
+// copyright(c)2022 Vishal Ahirwar.
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#define IMAGEFILES
-
-#ifdef IMAGEFILES
-#undef VIDEOFILES
-#elif VIDEOFILES
-#undef IMAGEFILES
-#endif
+// #define IMAGEFILES
+// #define VIDEOFILES
+// #define READVIDEOFILE
+// #define BASIC
 
 int main()
 {
 #ifdef IMAGEFILES
     const char *path{"../assets/images/v101.png"};
-    cv::Mat image = cv::imread(path);
+    cv::Mat image{cv::imread(path)};
     if (image.empty())
     {
         std::cout << "Image path is wrong or maybe file is correpted!\n";
@@ -41,9 +39,17 @@ int main()
             return -1;
         };
         cv::imshow("running front camera ", image);
-        if (cv::waitKey(5) == 27)
+        if (cv::waitKey(20) == 27)
             break;
     };
+#endif
+
+#ifdef BASIC
+    const char *path{"../assets/images/v101.png"};
+    cv::Mat img{cv::imread(path)}, imgGray;
+    cv::cvtColor(img, imgGray, cv::COLOR_RGB2GRAY);
+    cv::imshow("GRAY", imgGray);
+    cv::waitKey(0);
 #endif
     std::cout << "\nProgram Reached at the End!\n";
     return 0;
