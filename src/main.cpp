8733d04ada40ab8abf773c43273da2b64f0d2897
IVishalAhirwar <vishalchoudhry0091@gmail.com>
2022-01-31 00:42:48 +0530

    object detection using color and edges

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 // copyright(c)2022 Vishal Ahirwar.
 #include <iostream>
+#include<vector>
 #include <opencv2/opencv.hpp>
 // #define IMAGEFILES
 // #define VIDEOFILES
 // #define READVIDEOFILE
-// #define BASIC
+#define BASIC
+
+void GetContours(cv::Mat , cv::Mat );
 
 int main()
 {
@@ -45,12 +48,82 @@ int main()
 #endif
 
 #ifdef BASIC
-    const char *path{"../assets/images/v101.png"};
-    cv::Mat img{cv::imread(path)}, imgGray;
-    cv::cvtColor(img, imgGray, cv::COLOR_RGB2GRAY);
-    cv::imshow("GRAY", imgGray);
-    cv::waitKey(0);
+    const char *path{"../assets/images/color-shapes.png"};
+    /**
+        cv::Mat img{cv::imread(path)}, imgGray, imgCanny, imgDia, imgErode;
+        cv::cvtColor(img, imgGray, cv::COLOR_BGR2GRAY); // Gray
+        cv::GaussianBlur(img, imgGray, cv::Size(25, 25), 4, 0);
+        cv::Canny(imgGray, imgCanny, 25, 75);
+        cv::Mat Kernal = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
+        cv::dilate(imgCanny, imgDia, Kernal);
+        cv::erode(imgDia, imgErode, Kernal);
+        cv::namedWindow(path, cv::WINDOW_GUI_EXPANDED);
+        cv::imshow(path, imgErode);
+    **/
+    cv::Mat img, imgBlur, imgCanny;
+    // cv::VideoCapture cap(0);
+    /**
+        cv::resize(cv::imread(path),img,cv::Size(),0.7,0.5);
+
+        cv::rectangle(img,cv::Point(90,90),cv::Point(790,170),cv::Scalar(255,0,0),cv::FILLED);
+        cv::putText(img,"Hello, World to opencv C++ by Vishal.",cv::Point(100,130),cv::FONT_HERSHEY_TRIPLEX,1,cv::Scalar(255,255,255),1);
+        cv::imshow(path,img);
+    **/
+    /**
+         int hmin{}, smin{}, vmin{};
+         int hmax{}, smax{}, vmax{};
+
+         cv::namedWindow("Trackbars", (800, 200));
+         cv::cvtColor(img, imgHSV, cv::COLOR_RGB2HSV);
+         cv::createTrackbar("hue MIN", "Trackbars", &hmin, 255);
+         cv::createTrackbar("sat MIN", "Trackbars", &smin, 255);
+         cv::createTrackbar("val MIN", "Trackbars", &vmin, 255);
+         cv::createTrackbar("hue MAX", "Trackbars", &hmax, 255);
+         cv::createTrackbar("sat MAX", "Trackbars", &smax, 255);
+         cv::createTrackbar("val MAX", "Trackbars", &vmax, 255);
+
+         while (true)
+         {
+             // cap.read(img);
+
+             cv::Scalar lower(hmin, smin, vmin);
+             cv::Scalar upper(hmax, smax, vmax);
+             // printf("trackbars value : %d %d\n", hmin, hmax);
+             cv::inRange(img, lower, upper, mask);
+             cv::namedWindow(path, cv::WINDOW_GUI_EXPANDED);
+             cv::putText(imgHSV,"FRAMES HAS BEEN RUN :",cv::Point(hmin,hmax),cv::FONT_HERSHEY_DUPLEX,1,cv::Scalar(255,255,255));
+             cv::imshow(path, imgHSV);
+             if (cv::waitKey(1) == 27)
+                 break;
+         };
+     **/
+    cv::Mat src, imgDi,imgContours;
+    cv::VideoCapture cap(0);
+    cv::Mat Kernal{cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3))};
+    while (true)
+    {
+        cap.read(src);
+
+        cv::cvtColor(src, img, cv::COLOR_BGR2GRAY);
+        cv::GaussianBlur(img, imgBlur, cv::Size(3, 3), 3, 0);
+        cv::Canny(imgBlur, imgCanny, 25, 75);
+        cv::dilate(imgCanny, imgDi, Kernal);
+
+        GetContours(imgDi,imgContours);
+
+        cv::imshow(path, imgDi);
+        if (cv::waitKey(1) == 27)
+            break;
+    }
+
 #endif
-    std::cout << "\nProgram Reached at the End!\n";
     return 0;
 };
+void GetContours(cv::Mat img, cv::Mat out)
+{
+    std::vector<std::vector<cv::Point>> contours{};
+    std::vector<cv::Vec4i> hierarchy{};
+
+    cv::findContours(img,contours,hierarchy,cv::RETR_EXTERNAL,cv::CHAIN_APPROX_SIMPLE);
+    cv::drawContours(out,contours,-1,cv::Scalar(255,0,0),10);
+}
